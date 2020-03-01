e1c1b3d53b3bf0c088427397da0bdf13022e1efe
Lewis Brogan <34724891+iLewisBrogan@users.noreply.github.com>
2020-03-01 04:32:06 +0000

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "hello world"
+puts "hello again"
+puts "again"
+puts "again x2"
+puts "again x3"
+puts "again x4"
+puts "i'd 'not'"
+puts 'i "said".'
+#test
---
+++ ownEx4.rb
@@ -0,0 +1,5 @@
+numberOne = 4
+numberTwo = 4.0
+
+puts "numberOne: #{numberOne}"
+puts "numberTwo: #{numberTwo}"
